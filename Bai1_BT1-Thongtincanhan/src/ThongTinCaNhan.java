2b1b862ea920f48247c5a900f02cdab6e098d717
66133883 - JavaProgramming <tin.vt.66cntt@ntu.edu.vn>
2026-03-06 10:57:03 +0700

    Bài 1 - Bài Tập 1 Xong

---
+++ Bai1_BT1-Thongtincanhan/src/ThongTinCaNhan.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class ThongTinCaNhan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Em tên là: Võ Trọng Tín ");
+		System.out.println("MSSV: 66133883");
+		System.out.print("Lớp: 66.CNTT-3 ");
+
+	}
+
+}
