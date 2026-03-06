cce2fef412e2a36d3945a6f5c4e8603b971eabc8
66133883 - JavaProgramming <tin.vt.66cntt@ntu.edu.vn>
2026-03-06 11:34:32 +0700

    Create ViDuCuaThay.java
    Hoàn Thành Ví Dụ Của Thầy

---
+++ Bai1_BT4-CacViDuHello/src/ViDuCuaThay.java
@@ -0,0 +1,14 @@
+
+public class ViDuCuaThay {
+	public static void main(String[] args) {
+        // Text blocks với """
+        String greeting = """
+            ======================
+            HELLO, MODERN JAVA!
+            ======================
+            by Mai Cường Thọ NTU
+            ======================
+            """;
+        System.out.println(greeting);
+    }
+}
