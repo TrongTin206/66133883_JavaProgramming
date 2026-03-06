8d379a156bdcd4a4ae8b5ccb67c22c792a7683eb
66133883 - JavaProgramming <tin.vt.66cntt@ntu.edu.vn>
2026-03-06 11:34:49 +0700

    Create PhanTuSuyLuan.java
    Hoàn Thành Bài 4

---
+++ Bai1_BT4-CacViDuHello/src/PhanTuSuyLuan.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class PhanTuSuyLuan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// var - tự động suy luận kiểu
+        var message = "Hello, World with var!";
+        var number = 42;
+        var list = java.util.List.of("Java", "Modern", "Features");
+        
+        System.out.println(message);
+        System.out.println("Number: " + number);
+        System.out.println("List: " + list);
+	}	
+
+}
