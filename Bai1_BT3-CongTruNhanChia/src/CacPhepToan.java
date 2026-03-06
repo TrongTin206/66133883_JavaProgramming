c1e4e2fe5778c92905cca97fa1316ba6b6a56bf4
66133883 - JavaProgramming <tin.vt.66cntt@ntu.edu.vn>
2026-03-06 11:27:55 +0700

    Bài 1 - Bài tập 3

---
+++ Bai1_BT3-CongTruNhanChia/src/CacPhepToan.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class CacPhepToan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 4;
+		int b = 2;
+		System.out.println("<--- Các Phép Toán --->");
+
+		System.out.printf("Phép cộng: %d + %d = %d\n", a, b, a + b);
+		System.out.printf("Phép trừ : %d - %d = %d\n", a, b, a - b);
+		System.out.printf("Phép nhân: %d * %d = %d\n", a, b, a * b);
+		System.out.printf("Phép chia: %d / %d = %d\n", a, b, a / b);
+	}
+
+}
