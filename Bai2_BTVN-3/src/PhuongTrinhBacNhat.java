edc5204a6333f9d6c2e744d73802b034b58d7205
66133883 - JavaProgramming <tin.vt.66cntt@ntu.edu.vn>
2026-03-09 14:45:41 +0700

    Bài 2 - Bài tập về nhà 3

---
+++ Bai2_BTVN-3/src/PhuongTrinhBacNhat.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class PhuongTrinhBacNhat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 3;
+        int b = 6;
+
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phuong trinh vo so nghiem");
+            } else {
+                System.out.println("Phuong trinh vo nghiem");
+            }
+        } else {
+            int x = -b / a;
+            System.out.println("Nghiem x = " + x);
+        }
+	}
+}
