21d15b46c4ccb6349b1ac9822eea04063c2845d7
66133883 - JavaProgramming <tin.vt.66cntt@ntu.edu.vn>
2026-03-11 11:44:08 +0700

    Bài 3 - Bài Tập Về Nhà 3 ( Xong )

---
+++ Bai3_BTVN-2/src/QuyDoiTienTe.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+import java.util.Scanner;
+public class QuyDoiTienTe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập số tiền USD: ");
+		double USD = scanner.nextDouble();
+
+		double tyGia = 26245;
+		double VND = USD * tyGia;
+
+		System.out.printf("Số tiền USD: %.2f\n", USD);
+		System.out.printf("Quy đổi ra VND: %.2f", VND);
+
+		scanner.close();
+	}
+
+}
