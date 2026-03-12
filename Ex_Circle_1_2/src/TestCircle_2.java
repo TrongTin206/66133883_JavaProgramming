c9ae12f5744198788df10a2c2ef62c308d61616d
66133883 - JavaProgramming <tin.vt.66cntt@ntu.edu.vn>
2026-03-12 09:07:25 +0700

    Hàm main Ex_Circle_1_2

--- Ex_Circle_1_2/src/TestCircle_2.java
+++ Ex_Circle_1_2/src/TestCircle_2.java
@@ -1,4 +1,20 @@
 
-public class TestCircle_2 {
 
+public class TestCircle_2 {
+	 public static void main(String[] args) {
+
+	        Circle_2 c1 = new Circle_2(1.1);
+	        System.out.println(c1);
+
+	        Circle_2 c2 = new Circle_2();
+	        System.out.println(c2);
+
+	        c1.setRadius(2.2);
+	        System.out.println(c1);
+
+	        System.out.println("radius is: " + c1.getRadius());
+
+	        System.out.printf("area is: %.2f%n", c1.getArea());
+	        System.out.printf("circumference is: %.2f%n", c1.getCircumference());
+	 }
 }
