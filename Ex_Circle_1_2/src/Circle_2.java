54f70c4d8b77fc8ef8618678e6b6b7dea727f116
66133883 - JavaProgramming <tin.vt.66cntt@ntu.edu.vn>
2026-03-12 09:07:07 +0700

    Hàm Circle_1_2

--- Ex_Circle_1_2/src/Circle_2.java
+++ Ex_Circle_1_2/src/Circle_2.java
@@ -1,4 +1,26 @@
 
 public class Circle_2 {
 
+	    private double radius;
+	    public Circle_2() {
+	        radius = 1.0;
+	    }
+	    public Circle_2(double r) {
+	        radius = r;
+	    }
+	    public double getRadius() {
+	        return radius;
+	    }
+	    public void setRadius(double r) {
+	        radius = r;
+	    }
+	    public double getArea() {
+	        return radius * radius * Math.PI;
+	    }
+	    public double getCircumference() {
+	        return 2 * Math.PI * radius;
+	    }
+	    public String toString() {
+	        return "Circle[radius=" + radius + "]";
+	}
 }
