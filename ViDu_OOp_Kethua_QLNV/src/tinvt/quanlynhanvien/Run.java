291e232db6724178045f025b5b9070ee2d64591b
66133883 - JavaProgramming <tin.vt.66cntt@ntu.edu.vn>
2026-03-19 15:04:11 +0700

    Hoàn Thành Bài
    Added main method to create and display employee information.

--- ViDu_OOp_Kethua_QLNV/src/tinvt/quanlynhanvien/Run.java
+++ ViDu_OOp_Kethua_QLNV/src/tinvt/quanlynhanvien/Run.java
@@ -1,5 +1,13 @@
 package tinvt.quanlynhanvien;
 
 public class Run {
-
+	public static void main(String[] args) {
+		NhanVien nv1 = new NhanVien("001" , "Mai Cường Thọ" , 2300000);
+//		nv1.hienThiThongTin();
+        System.out.println(nv1.toString());
+        
+        NhanVienVanPhong nvVanPhong1 = new NhanVienVanPhong("002" , "Trần Thị Tươi Thắm" , 2300000 , 3 , 200000);
+//        System.out.println(nvVanPhong1.toString());
+        nvVanPhong1.hienThiThongTin();
+	} 
 }
