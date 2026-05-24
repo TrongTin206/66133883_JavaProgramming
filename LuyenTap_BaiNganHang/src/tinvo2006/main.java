5d7d27d8f98e1deb15f621fc97c61e1e95066047
66133883 - JavaProgramming <tin.vt.66cntt@ntu.edu.vn>
2026-05-24 12:04:34 +0700

    Luyện thêm

---
+++ LuyenTap_BaiNganHang/src/tinvo2006/main.java
@@ -0,0 +1,8 @@
+package tinvo2006;
+
+public class main {
+public static void main(String[] args) {	
+	KhachHang kh1 = new KhachHang("KH001","Nguyễn Khánh Ngân","0981003057","13 Lê Lợi, TP. Thủ Đức, TPHCM");
+		System.out.println(kh1.hienThi());
+	}
+}
