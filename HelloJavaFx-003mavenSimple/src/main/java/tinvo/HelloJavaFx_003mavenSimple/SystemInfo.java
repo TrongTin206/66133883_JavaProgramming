e00dcff63be61076b1ae7a8100c584bb0edd580e
66133883 - JavaProgramming <tin.vt.66cntt@ntu.edu.vn>
2026-05-25 15:29:43 +0700

    Maven Simple

---
+++ HelloJavaFx-003mavenSimple/src/main/java/tinvo/HelloJavaFx_003mavenSimple/SystemInfo.java
@@ -0,0 +1,13 @@
+package tinvo.HelloJavaFx_003mavenSimple;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
